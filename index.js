d095f7e79d0b3081dc1d142be5a836680ea6b897
imran-hossain <engg.imran22@gmail.com>
2024-01-02 20:35:13 +0600

    image save using multer

--- index.js
+++ index.js
@@ -1,7 +1,60 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path')
+const mongoose = require('mongoose');
+const { log } = require('console');
 
+mongoose
+    .connect("mongodb://localhost/todos")
+    .then(()=>console.log("Connection successfully"))
+    .catch((err)=>console.log(err))
 const app = express();
-app.set('view engine','ejs')
+const UPLOADS_FOLDER = './uploads';
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, UPLOADS_FOLDER);
+    },
+    filename: (req, file, cb) => {
+        const fileExt = path.extname(file.originalname);
+        const fileName = file.originalname.replace(fileExt, '').toLowerCase().split(' ').join('-');
+        `-${Date.now()}`;
+        cb(null, fileName + fileExt);
+    },
+});
+const upload = multer({
+    storage,
+    limits: {
+        fileSize: 1000000,
+    },
+    fileFilter: (req, file, cb) => {
+        if (
+            file.mimetype === 'image/png'
+            || file.mimetype === 'image/jpg'
+            || file.mimetype === 'image/jpeg'
+        ) {
+            cb(null, true);
+        } else {
+            cb(new Error('Only .jpg , .png and .jpeg format is allowed'));
+        }
+    },
+});
+
+app.post('/fileupload', upload.array('avatar', 2), (req, res) => {
+    res.send('Upload successfullay');
+});
+
+app.use((err, req, res, next) => {
+    if (err) {
+        if (err instanceof multer.MulterError) {
+            res.status(500).send('There was an upload error');
+        } else {
+            res.status(500).send(err.message);
+        }
+    } else {
+        res.send('success');
+    }
+});
+app.set('view engine', 'ejs');
 // const admin = express();
 
 // app.use(express.json());
